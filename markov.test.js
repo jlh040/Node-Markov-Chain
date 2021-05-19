d9a12aa96dd44e33e1bf9ed821169f671bbc0118
Jeff Hall <jeff.leon.hall@gmail.com>
2021-05-18 17:53:24 -0700

    wrote tests to check the properties and methods of MarkovMachine

--- markov.test.js
+++ markov.test.js
@@ -2,10 +2,21 @@ const { MarkovMachine } = require('./markov');
 
 describe('MarkovMachine Tests', () => {
     test('can we make an instance of the class?', () => {
-        let markovian = new MarkovMachine('the cat is in the hat')
+        let markovian = new MarkovMachine('the cat is in the hat');
         expect(markovian).toBeInstanceOf(MarkovMachine);
 
-        let markovianV2 = new MarkovMachine('a')
+        let markovianV2 = new MarkovMachine('a');
         expect(markovianV2).toBeInstanceOf(MarkovMachine);
+    })
+
+    test('does an instance have the properties and methods we expect?', () => {
+        let markovianV3 = new MarkovMachine('where in the world is carmen san diego');
+        expect(markovianV3.words).toEqual(expect.any(Array));
+
+        let markovianV4 = new MarkovMachine('Sam I am that is my name');
+        expect(markovianV4.makeChains).toBeInstanceOf(Function);
+
+        let markovianV5 = new MarkovMachine('The world is a big place and it is not a race.');
+        expect(markovianV5.makeText).toBeInstanceOf(Function);
     })
 })
