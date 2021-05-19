7ae0aa3d1ba53c2dedb2d9d1201cdb61b1b7fbe9
Jeff Hall <jeff.leon.hall@gmail.com>
2021-05-18 17:59:36 -0700

    wrote tests to make sure we get text back from makeText

--- markov.test.js
+++ markov.test.js
@@ -19,4 +19,12 @@ describe('MarkovMachine Tests', () => {
         let markovianV5 = new MarkovMachine('The world is a big place and it is not a race.');
         expect(markovianV5.makeText).toBeInstanceOf(Function);
     })
+
+    test('Do we get an actual string of text from makeText?', () => {
+        let markovianV6 = new MarkovMachine('There are no eggs like the eggs that I am eating');
+        expect(markovianV6.makeText()).toEqual(expect.any(String));
+
+        let markovianV7 = new MarkovMachine('This is one of the largest dr seuss books in the world and it is great');
+        expect(markovianV6.makeText()).toEqual(expect.any(String));
+    })
 })
